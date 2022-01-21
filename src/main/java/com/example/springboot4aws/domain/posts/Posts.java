a65a08516140a614679bd2998205b0a043dee497
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2022-01-21 14:11:55 +0900

    posts

---
+++ src/main/java/com/example/springboot4aws/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.example.springboot4aws.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor //기본 생성자 자동 추가 , public Posts(){}와 같은 효과
+@Entity //JPA 어노테이션
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder// 해당 클래스의 빌더 패턴 클래스 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
